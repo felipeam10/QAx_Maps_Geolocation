ca5f06d853b3dac755404949431c0fb18027042c
felipeam10 <felipeam10@hotmail.com>
2026-01-20 10:02:32 -0300

    adjusts

--- projects/cypress-xp/hope/web/cypress/e2e/register.cy.js
+++ projects/cypress-xp/hope/web/cypress/e2e/register.cy.js
@@ -39,21 +39,3 @@ describe('Cadastro Orfanatos', () => {
 
   });
 });
-
-Cypress.Commands.add('setMapPosition', (position) => {
-  window.localStorage.setItem('hope-qa:latitude', position.latitude);
-  window.localStorage.setItem('hope-qa:longitude', position.longitude);
-});
-
-Cypress.Commands.add('visitWithMockGeolocation', (url, latitude = -23.5276158, longitude = -46.6810411) => {
-  const mockGeolocation = (win, latitude, longitude) => {
-    cy.stub(win.navigator.geolocation, 'getCurrentPosition', cb => {
-      return cb({ coords: { latitude, longitude } });
-    });
-  };
-  cy.visit(url, {
-    onBeforeLoad: win => {
-      mockGeolocation(win, latitude, longitude);
-    }
-  });
-});
--- projects/cypress-xp/hope/web/cypress/support/commands.js
+++ projects/cypress-xp/hope/web/cypress/support/commands.js
@@ -1,25 +1,17 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('setMapPosition', (position) => {
+  window.localStorage.setItem('hope-qa:latitude', position.latitude);
+  window.localStorage.setItem('hope-qa:longitude', position.longitude);
+});
+
+Cypress.Commands.add('visitWithMockGeolocation', (url, latitude = -23.5276158, longitude = -46.6810411) => {
+  const mockGeolocation = (win, latitude, longitude) => {
+    cy.stub(win.navigator.geolocation, 'getCurrentPosition', cb => {
+      return cb({ coords: { latitude, longitude } });
+    });
+  };
+  cy.visit(url, {
+    onBeforeLoad: win => {
+      mockGeolocation(win, latitude, longitude);
+    }
+  });
+});
