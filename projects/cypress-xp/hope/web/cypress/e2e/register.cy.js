d2b6e70590445cc7bb6bcbba65541179264f48e3
felipeam10 <felipeam10@hotmail.com>
2026-01-22 01:41:59 -0300

    inserting duplicated orphanages

--- projects/cypress-xp/hope/web/cypress/e2e/register.cy.js
+++ projects/cypress-xp/hope/web/cypress/e2e/register.cy.js
@@ -38,4 +38,68 @@ describe('Cadastro Orfanatos', () => {
         .should('have.text', 'Orfanato cadastrado com sucesso.');
 
   });
+
+  it.only('should not to register a mew orphanage when his name is duplicated', () => {
+    const orphanageData = data.duplicate;
+
+    cy.deleteMany({name: orphanageData.name}, {collection: 'orphanages'});
+
+    // First registration
+    cy.visitWithMockGeolocation('http://localhost:3000/orphanages/create');
+    cy.get('legend')
+        .should('be.visible')
+        .should('have.text', 'Cadastro');
+
+    cy.setMapPosition(orphanageData.position);
+    
+    cy.get('input[name="name"]')
+        .type(orphanageData.name);
+    
+    cy.get('#description')
+        .type(orphanageData.description);
+
+    cy.get('input[type="file"]')
+        .selectFile('cypress/fixtures/images/kids-playground-1.png', { force: true });
+
+    cy.get('#opening_hours')
+        .type(orphanageData.opening_hours);
+
+    cy.contains('button', orphanageData.open_on_weekends)
+        .click();
+    
+    cy.get('.save-button')
+        .click();
+
+    //second registration with the same name
+    cy.wait(5000); //wait for 2 seconds to avoid overlapping alerts
+    cy.visitWithMockGeolocation('http://localhost:3000/orphanages/create');
+    cy.get('legend')
+        .should('be.visible')
+        .should('have.text', 'Cadastro');
+
+    cy.setMapPosition(orphanageData.position);
+    
+    cy.get('input[name="name"]')
+        .type(orphanageData.name);
+    
+    cy.get('#description')
+        .type(orphanageData.description);
+
+    cy.get('input[type="file"]')
+        .selectFile('cypress/fixtures/images/kids-playground-1.png', { force: true });
+
+    cy.get('#opening_hours')
+        .type(orphanageData.opening_hours);
+
+    cy.contains('button', orphanageData.open_on_weekends)
+        .click();
+    
+    cy.get('.save-button')
+        .click();
+
+    cy.get('.swal2-html-container')
+        .should('be.visible')
+        .should('have.text', 'Já existe um cadastro com o nome: ' + orphanageData.name);
+
+  });
 });
